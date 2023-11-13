2683faefbddcb5d3e4e442f29ba0e0575ee7a836
an.tsarev <an.tsarev@corp.vk.com>
2023-11-13 18:05:07 +0300

    Добавил стиль кнопкам и убрал модальное окно.

--- game.js
+++ game.js
@@ -1,56 +1,56 @@
 function playGame(playerChoice) {
-	highlightChoice(playerChoice);
-	showSpinner();
+  highlightChoice(playerChoice);
+  showSpinner();
 
-	setTimeout(() => {
-		const choices = ['камень', 'ножницы', 'бумага'];
-		const computerChoice = choices[Math.floor(Math.random() * 3)];
-		hideSpinner();
-		removeHighlight(playerChoice);
+  setTimeout(() => {
+    const choices = ["камень", "ножницы", "бумага"];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    hideSpinner();
+    removeHighlight(playerChoice);
 
-		showResultModal('Принимаем ваши ставки...');
-		setTimeout(() => {
-			const winner = determineWinner(playerChoice, computerChoice);
-			showResultModal(`Ставки сделаны, победитель: ${winner}`);
-		}, 2000);
-	}, 2000);
+    showResultModal("Принимаем ваши ставки...");
+    setTimeout(() => {
+      const winner = determineWinner(playerChoice, computerChoice);
+      showResultModal(`Ставки сделаны, победитель: ${winner}`);
+    }, 2000);
+  }, 2000);
 }
 
 function determineWinner(playerChoice, computerChoice) {
-	if (playerChoice === computerChoice) {
-		return 'Ничья';
-	} else if (
-		(playerChoice === 'камень' && computerChoice === 'ножницы') ||
-		(playerChoice === 'ножницы' && computerChoice === 'бумага') ||
-		(playerChoice === 'бумага' && computerChoice === 'камень')
-	) {
-		return 'Игрок';
-	} else {
-		return 'Компьютер';
-	}
+  if (playerChoice === computerChoice) {
+    return "Ничья";
+  } else if (
+    (playerChoice === "камень" && computerChoice === "ножницы") ||
+    (playerChoice === "ножницы" && computerChoice === "бумага") ||
+    (playerChoice === "бумага" && computerChoice === "камень")
+  ) {
+    return "Игрок";
+  } else {
+    return "Компьютер";
+  }
 }
 
 function highlightChoice(choice) {
-	document.getElementById(`image-${choice}`).classList.add('active');
+  document.getElementById(`image-${choice}`);
 }
 
 function removeHighlight(choice) {
-	document.getElementById(`image-${choice}`).classList.remove('active');
+  document.getElementById(`image-${choice}`);
 }
 
 function showSpinner() {
-	document.getElementById('spinner').style.display = 'block';
+  document.getElementById("spinner").style.display = "block";
 }
 
 function hideSpinner() {
-	document.getElementById('spinner').style.display = 'none';
+  document.getElementById("spinner").style.display = "none";
 }
 
 function showResultModal(message) {
-	const resultModal = document.getElementById('result-modal');
-	resultModal.innerText = message;
-	resultModal.style.display = 'block';
-	setTimeout(() => {
-		resultModal.style.display = 'none';
-	}, 4000);
+  const resultModal = document.getElementById("result-modal");
+  resultModal.innerText = message;
+  resultModal.style.display = "block";
+  setTimeout(() => {
+    resultModal.style.display = "none";
+  }, 4000);
 }
